842cef436f3222a0a3b82061df90bfda13b7ecf7
Saloni Chandra Pal Singh Chauhan <40295823+salonichauhan1101@users.noreply.github.com>
2025-07-17 12:36:07 -0400

    Create Longest_Consecutive_Sequence.java

---
+++ Arrays/Longest_Consecutive_Sequence.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        int n=nums.length;
+        if(n==0) return 0;
+
+        Arrays.sort(nums);
+        int cnt=0;
+        int max=1;
+        int smallerNum=Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            if(nums[i]-1 == smallerNum){
+                cnt++;
+                smallerNum=nums[i];
+            }
+            else if(nums[i]!=smallerNum){
+            cnt=1;
+            smallerNum=nums[i];
+            }
+            max=Math.max(max,cnt);
+
+        }
+        return max;
+    }
+}
