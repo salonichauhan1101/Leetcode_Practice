f795b71d3c065ab20743cd705783ccd045a0dd5e
Saloni Chandra Pal Singh Chauhan <40295823+salonichauhan1101@users.noreply.github.com>
2025-07-20 09:23:51 -0400

    Create Container_with_most_water.java

---
+++ Arrays/Container_with_most_water.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int n=height.length;
+        int left=0;
+        int right=n-1;
+        int maxArea=0;
+
+        while(left<right){
+            maxArea=Math.max(maxArea,(right-left)* Math.min(height[left],height[right]));
+            if(height[left]>height[right]){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        return maxArea;
+    }
+}
