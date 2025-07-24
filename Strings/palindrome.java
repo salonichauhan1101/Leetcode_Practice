8fdf277b00d00fd3064da64c5e99eb64c518f0b8
Saloni Chandra Pal Singh Chauhan <40295823+salonichauhan1101@users.noreply.github.com>
2025-07-24 16:28:35 -0400

    Create palindrome.java

---
+++ Strings/palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+       
+        s=s.toLowerCase().replaceAll("[^a-z0-9]","");
+
+        int p1=0;
+        int p2=s.length()-1;
+
+        while(p1<p2){
+            if(s.charAt(p1)!=s.charAt(p2)){
+                return false;
+            }
+            p1++;
+            p2--;
+        }
+        return true;
+
+    }
+}
