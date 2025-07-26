555ea3f4bdb5d4eb2743f8891d6e0549cb427965
Saloni Chandra Pal Singh Chauhan <40295823+salonichauhan1101@users.noreply.github.com>
2025-07-26 09:39:44 -0400

    Create ReverseNum.java

---
+++ Basics/ReverseNum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+        int rev=0;
+        int temp=0;
+
+        while(x!=0){
+            temp=x%10;
+
+            if(rev > Integer.MAX_VALUE/10 || rev < Integer.MIN_VALUE/10){
+                return 0;
+            }
+            rev=temp+rev*10;
+            x=x/10;
+        }
+        return rev;
+    }
+}
