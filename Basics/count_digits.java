ad8418ed192e1480eaa87d391bf008542f9eff31
Saloni Chandra Pal Singh Chauhan <40295823+salonichauhan1101@users.noreply.github.com>
2025-08-19 16:36:54 -0400

    Update count_digits.java

--- Basics/count_digits.java
+++ Basics/count_digits.java
@@ -1,15 +1,18 @@
 class Main {
     public static void main(String[] args) {
-        int num=983021;
-        int count=0;
-        
-        while(num!=0){
-            int temp=num%10;
-            if(temp>=0){
-                count++;
-            }
-            num=num/10;
-        }
-        System.out.println(count);
+       int num=980315;
+       int count=0;
+       
+       if(num==0){
+           count=1;
+       }
+       else{
+           num=Math.abs(num);
+           while(num!=0){
+               count++;
+               num=num/10;
+           }
+       }
+       System.out.println(count);
     }
 }
